c89a46182b596c3d5acd3b315bd92e542119b8e4
ABBY\lforv <13773011@scu.edu.tw>
2024-11-20 23:09:40 +0800

    調整程式順序

--- New.py
+++ New.py
@@ -41,63 +41,53 @@ def callback():
 
     return 'OK'
 
+# 建立一個函式來處理特殊詞彙
+def handle_special_words(sentence):
+    for word in special_words:
+        if word in sentence: # 遇到特殊詞彙就 return None
+            return None  
+        else: 
+            return 0
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
         user_id = event.source.user_id
         questionSentance = event.message.text  # 使用者啟動對話之內容
-        # 檢查是否包含特殊詞彙
-        for word in special_words:
-            if word in questionSentance:
-                return  # 遇到特殊詞彙就 return，使用預設回覆
+        result = handle_special_words(questionSentance)
+        if result is None:    
+            noneMessage ='您好！請問您想了解哪方面的資訊呢？'
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=noneMessage))
 
-        jiebaQuestionList = jieba.cut(questionSentance)
-        # text_message = ' | '.join(jiebaQuestionList)  # 將生成器轉換為字串
-
-        # 載入 jieba 詞典
-        jieba.initialize()
-        # 斷詞
-        words = jieba.cut(questionSentance)
-        dictionary_words = set(jieba.dt.FREQ.keys())  # 獲取jieba詞典
-
-        with open('Real_userdict_1.txt', 'r', encoding='utf-8') as f:  # 加入Real_userdict_1.txt 中的詞彙
-            for line in f:
-                word = line.strip().split(' ')[0]
-                dictionary_words.add(word)
-
-        # 篩選出字典中存在的詞彙
-        matched_words = [word for word in words if word in dictionary_words]
-        dictWords = ("|".join(matched_words))  # 輸出匹配的詞彙
-        confirmMessate = '原來你對' + matched_words[0] + "有興趣呀?"
-        message1 = TextSendMessage(text=confirmMessate)
-        splitWords = dictWords.split('|')
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=confirmMessate))
-        # 取得 splitWords[0] 欄位的所有唯一值
-        matched_values = df[splitWords[0]].unique()
-        # print(confirmMessate)
-
-        ## 迭代所有 matched_values，找到符合條件的列
-        matched_rows = pd.DataFrame()  # 建立一個空的 DataFrame 來儲存所有匹配的列
-        
-        for matched_value in matched_values:
-            matched_row = df[df[splitWords[0]] == matched_value]  # 找到符合條件的列
-            matched_rows = pd.concat([matched_rows, matched_row])  # 將匹配的列加入 matched_rows
-        
-        confirmMessate = confirmMessate+':' +matched_rows[0]
-       
-        # 使用 dictWords 搜尋符合的資料
-        if not matched_row.empty:
-            # 取得第一筆符合的資料
-            first_match = matched_row.iloc[0]  
-            # 將 Series 轉換為 dict
-            feedback = first_match.to_dict()
-            # 篩選掉 value 為 nan 的 key-value pairs
-            feedback = {key: value for key, value in feedback.items() if pd.notna(value)}  
-            feedback_str = "Carmin小幫手推薦您您可能對這張信用卡感興趣:\n" + "\n".join([f"{key}: {value}" for key, value in feedback.items()])  # 組合回覆訊息
-            message2 = TextSendMessage(text=feedback_str)  # 將 feedback_str 轉換為 TextSendMessage 物件
-            line_bot_api.reply_message(event.reply_token,[message1,message2])
         else:
-            line_bot_api.reply_message(event.reply_token, [message1, "抱歉，找不到符合您需求的信用卡"])
+            jiebaQuestionList = jieba.cut(questionSentance) # text_message = ' | '.join(jiebaQuestionList)  # 將生成器轉換為字串
+            jieba.initialize() # 載入 jieba 詞典        
+            words = jieba.cut(questionSentance)# 斷詞
+            dictionary_words = set(jieba.dt.FREQ.keys())  # 獲取jieba詞典
+            with open('Real_userdict_1.txt', 'r', encoding='utf-8') as f:  # 加入Real_userdict_1.txt 中的詞彙
+                for line in f:
+                    word = line.strip().split(' ')[0]
+                    dictionary_words.add(word)
+            matched_words = [word for word in words if word in dictionary_words] # 篩選出字典中存在的詞彙
+            dictWords = ("|".join(matched_words))  # 輸出匹配的詞彙
+            confirmMessate = '原來你對' + matched_words[0] + "有興趣呀?"
+            message1 = TextSendMessage(text=confirmMessate)
+            splitWords = dictWords.split('|')
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=confirmMessate))
+            matched_values = df[splitWords[0]].unique() # 取得 splitWords[0] 欄位的所有唯一值
+            matched_rows = pd.DataFrame()  # 建立一個空的 DataFrame 來儲存所有匹配的列
+            for matched_value in matched_values:## 迭代所有 matched_values，找到符合條件的列
+                matched_row = df[df[splitWords[0]] == matched_value]  # 找到符合條件的列
+                matched_rows = pd.concat([matched_rows, matched_row])  # 將匹配的列加入 matched_rows      
+            confirmMessate = confirmMessate+':' +matched_rows[0]      
+            if not matched_row.empty: # 使用 dictWords 搜尋符合的資料          
+                first_match = matched_row.iloc[0]  # 取得第一筆符合的資料
+                feedback = first_match.to_dict() # 將 Series 轉換為 dict                           
+                feedback = {key: value for key, value in feedback.items() if pd.notna(value)}  # 篩選掉 value 為 nan 的 key-value pairs
+                feedback_str = "Carmin小幫手推薦這張信用卡:\n" + "\n".join([f"{key}: {value}" for key, value in feedback.items()])  # 組合回覆訊息
+                message2 = TextSendMessage(text=feedback_str)  # 將 feedback_str 轉換為 TextSendMessage 物件
+                line_bot_api.reply_message(event.reply_token,[message1,message2])
+            else:
+                line_bot_api.reply_message(event.reply_token, [message1, "抱歉，找不到符合您需求的信用卡"])
 
 
     except Exception as e:
