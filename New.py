9eb0aa092d8614e1f1a2f50f259b75121d597fe8
RedOrangeSCU <13773011@scu.edu.tw>
2024-11-20 17:05:36 +0800

    Update New.py

--- New.py
+++ New.py
@@ -89,7 +89,7 @@ def handle_message(event):
                 feedback_str = "Carmin小幫手推薦您您可能對這張信用卡感興趣:\n" + "\n".join([f"{key}: {value}" for key, value in feedback.items()])  # 組合回覆訊息
                 print(feedback_str)
                 message2 = TextSendMessage(text=feedback_str)  # 將 feedback_str 轉換為 TextSendMessage 物件
-                line_bot_api.reply_message(event.reply_token, [message1, message2])
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message1))
             else:
                 line_bot_api.reply_message(event.reply_token, [message1, "抱歉，找不到符合您需求的信用卡"])
 
