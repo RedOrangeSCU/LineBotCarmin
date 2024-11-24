d0a09959aa518b7443fa58c382656cfe15e9068a
ABBY\lforv <13773011@scu.edu.tw>
2024-11-25 01:11:37 +0800

    調整套件版本

--- mongodb_function.py
+++ mongodb_function.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import CSVLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
+
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import MongoDBAtlasVectorSearch
 
 # 從環境變數中獲取 MongoDB URI
